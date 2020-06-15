f545a28c1dcf4f621634092717a23440ef9c0ba1
choibongyong <58247736+choibongyong@users.noreply.github.com>
2020-06-15 12:15:29 +0900

    inin

--- src/hello/start5.java
+++ src/hello/start5.java
@@ -7,15 +7,30 @@ public class start5 {
 		
 		
 		String account;
-		String result;
+		String Nunber;
+		int am;
 		Scanner account_input = new Scanner(System.in);
+		Scanner amount = new Scanner(System.in);
 		
-		System.out.print("계좌번호와 예금주를 입력하세요 : ");
+		System.out.print("(계좌번호,예금주)를 입력하세요 : ");
 		account = account_input.next();
-		result = account.substring(account.lastIndexOf(","+1));
-				
-		System.out.println(account);
-		System.out.println(result);
+		
+		Nunber = account.substring(0,account.indexOf(","));
+		account = account.substring(account.indexOf(",")+1);
+		
+		//System.out.println(Nunber);
+		//System.out.println(account);
+		
+		System.out.print("입금하실 금액를 입력하세요 : ");
+		am = amount.nextInt();
+		
+		BankAccount db = new BankAccount(Nunber, account, am);
+		db.printStatus();
+		db.deposit(20000);
+		db.withDraw(45000);
+		
+		
+		
 		account_input.close();
 	}
 
